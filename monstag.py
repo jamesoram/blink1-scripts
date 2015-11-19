37a935831c2f6da08247a652672553ba417f6d7f
James Oram <herespam1@hotmail.com>
2015-11-19 23:35:40 +0000

    wip

--- monstag.py
+++ monstag.py
@@ -1,36 +1,42 @@
 import urllib2
 import time
+import Queue
 
-class Monitor:
+class Monitor(object):
 
-  def __init__(self):
-    self.url = "http://chhlapphot006.karmalab.net:7405/header_svc/version.txt"
-    self.expected = "release.tag"
-    self.colour = "12AA3F"
-    self.blink_time = "700"
+    def __init__(self):
+        self.url = "http://chhlapphot006.karmalab.net:7405/header_svc/version.txt"
+        self.expected = "release.tag"
+        self.colour = "12AA3F"
+        self.blink_time = "700"
 
-  def check(self):
-    response = urllib2.urlopen(self.url).read()
-    return self.expected in response
+    def check(self):
+        response = urllib2.urlopen(self.url).read()
+        return self.expected in response
 
-class Blinker:
+class Blinker(object):
 
-  def __init__(self):
-    q = Queue.LifoQueue()
-    command = "blink1-tool --rgb "
+    def __init__(self):
+        self.queue = Queue.LifoQueue()
+        self.command = "blink1-tool --rgb "
 
-  def add(monitor):
-    q.put(monitor)
-    command.run(q.get().colour + " -t " + q.get().blink_time)
-    
-class Poller:
+    def add(self, monitor):
+        self.queue.put(monitor)
+        self.command.run(self.queue.get().colour + " -t " + self.queue.get().blink_time)
 
-  def __init__(self):
-    self.poll_time = 90
+class Poller(object):
 
-  def poll(self):
-    for monitor in monitors:
-      if not monitor.check
-        blinker.add(monitor)
-    time.sleep(poll_time)
+    def __init__(self):
+        self.poll_time = 90
+        self.monitors = []
+        self.monitors.append(Monitor())
+        self.blinker = Blinker()
 
+    def poll(self):
+        for monitor in self.monitors:
+            if not monitor.check():
+                self.blinker.add(monitor)
+        time.sleep(self.poll_time)
+
+poller = Poller()
+poller.poll()
